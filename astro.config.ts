d61152518f0b293deb77c49e8493e3110494862c
Steve Paulson <staevage@gmail.com>
2026-02-05 09:50:59 +0100

    WIP

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,6 @@
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import partytown from "@astrojs/partytown";
-import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import playformInline from "@playform/inline";
 import sentry from "@sentry/astro";
@@ -25,7 +24,6 @@ export default defineConfig({
   }),
   integrations: [
     mdx(),
-    react(),
     sitemap(),
     favicons({
       input: {
